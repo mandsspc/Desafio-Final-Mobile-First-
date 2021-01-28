53d7ce0206c3c050a2ce0337940faf254cac3be3
vainaweb <vainaweb@gmail.com>
2021-01-28 16:52:53 -0300

    Finalização da main no responsive

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
+    if (x.style.display === "none") {
       x.style.display = "block";
+    } else {
+      x.style.display = "none";
     }
   }
+
+  /* var visibilidade = true
+  function aparecerocultar() {
+    if (visibilidade) {
+      document.getElementById("div1").style.display = "none";
+      visibilidade = false
+    } else {
+      document.getElementById("div1").style.display = "block";
+      visibilidade = true
+    }
+  } */
